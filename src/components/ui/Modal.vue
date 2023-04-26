81deb1bac88cc4823e3e051230aee3b6799fbc8a
chisom promise <chimisepro@gmail.com>
2023-04-26 12:34:21 +0100

    ui: Refactor Modal styles

--- src/components/ui/Modal.vue
+++ src/components/ui/Modal.vue
@@ -27,7 +27,7 @@ const emits = defineEmits<Emits>()
                     enter-to-class="opacity-100" leave-active-class="transition-all duration-200 ease-in-out"
                     leave-from-class="opacity-100 scale-100" leave-to-class="opacity-95 scale-100">
                     <div
-                        class="absolute top-0 left-0 min-h-full w-full flex items-center p-9 justify-center bg-black/10 pointer-events-none">
+                        class="absolute top-0 left-0 min-h-full w-full flex items-center p-5 justify-center bg-black/10 pointer-events-none">
                         <div class="bg-white w-full shadow-md max-w-[580px] md:w-7/12 pointer-events-auto">
                             <slot></slot>
                         </div>
