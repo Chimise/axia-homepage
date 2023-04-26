fdf1c27a977339dd60942595c29ab7bf3c4c0b2b
chisom promise <chimisepro@gmail.com>
2023-04-26 10:29:41 +0100

    ui: Create onboarding section for the homepage

---
+++ src/components/common/BoardingSection.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import Quote from '../icons/Quote.vue';
+</script>
+
+<template>
+    <section class="bg-primary py-14 md:py-32 text-white">
+        <div class="axia-container axia-gutter">
+            <header class="font-medium text-3xl md:text-4xl">
+                <h3>Get Onboard with Us</h3>
+            </header>
+            <div class="flex flex-wrap-reverse mt-10 md:-mx-4 md:mt-14">
+                <div class="py-5 w-full md:p-4 md:w-7/12">
+                    <img src="/onboard.svg" alt="Onboard Image" class="object-cover w-full h-full" />
+                </div>
+                <div class="py-5 w-full space-y-8 md:pl-12 md:pr-4 md:w-5/12 leading-7">
+                    <div>
+                        <Quote class="text-white w-14 h-14" />
+                    </div>
+                    <p>Axia Africa is an organization that is focused on equipping African youths with the knowledge and
+                        digital
+                        skills needed today Our aim is to make tech knowledge accessible to every individual irrespective of
+                        age,
+                        literacy, and location.</p>
+                    <p>We are looking forward to building a strong community of ready-to-work personnel and employable
+                        candidates
+                        around the world in the tech space.</p>
+                </div>
+            </div>
+            <div class="space-y-6 md:space-y-7">
+                <div class="flex items-center space-x-4 mt-5 md:mt-7">
+                    <span class="w-20 h-1 bg-white"></span>
+                    <h6 class="font-medium text-3xl">Why Choose Us</h6>
+                </div>
+                <p class="leading-7">
+                    We at Axia Africa are intentional about our students and focused about building a community. The tech
+                    space in
+                    Africa is fast growing and we have the necessary programmes to help launch you into that space. We
+                    sharpen
+                    your skills to give you leverage in your career while navigating the job market
+                </p>
+        </div>
+    </div>
+</section></template>
